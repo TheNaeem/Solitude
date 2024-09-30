1ff3bad4a6a18f90c1fdae23042937d8e7c8e1f9
ᴅᴊʟᴏʀ3xᴢᴏ <lorenzogame85@gmail.com>
2024-09-30 03:30:21 +0200

    implement client options in the inherited class

--- Solitude/Objects/Endpoints/EpicManifestEndpoint.cs
+++ Solitude/Objects/Endpoints/EpicManifestEndpoint.cs
@@ -1,12 +1,17 @@
-﻿using Solitude.Objects.Auth;
+﻿using RestSharp;
+using Solitude.Objects.Auth;
 
 namespace Solitude.Objects.Endpoints;
 
 public class EpicManifestEndpoint : DefaultEndpoint // took these endpoint models from Nightwatcher, another project of mine
 {
-    public EpicManifestEndpoint(string manifestPath)
-        : base($"https://launcher-public-service-prod06.ol.epicgames.com/{manifestPath}")
+    private static readonly RestClientOptions _options = new()
     {
-        Client.Authenticator = new EpicLauncherAuthenticator();
+        Authenticator = new EpicLauncherAuthenticator()
+    };
+
+    public EpicManifestEndpoint(string manifestPath)
+        : base($"https://launcher-public-service-prod06.ol.epicgames.com/{manifestPath}", options: _options)
+    {
     }
 }
